7ea658b4356c0cffb1ffb0c58c3289fbd8fab4a7
Bloodymars <71287749+Kirilltl@users.noreply.github.com>
2024-10-01 17:57:01 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,17 +1,3 @@
-//for client and server(ping only localhost):
-    //Edit Configurations:
-        //-size 1024 -p 50001 -h localhost -f "C:\test.txt"
-        //-size 1024 -p 50001 -h localhost -f "C:\Program Files\Wireshark\Wireshark.exe"
-//for only client(ping sites):
-    //Edit Configurations:
-        //-size 1024 -p 443 -h 208.67.220.220
-    //IP / port:
-        //208.67.220.220 / 443
-        //vk.com / 443, 33434
-        //youtube.com / 443, 33434
-//Functions:
-    //sendPackageFromFileToServer() - отправляет пакеты, считывая с указанного файла
-    //sendPackageToServer() - отправляет пакеты, указанные в массиве sendData
 package org.example;
 import org.apache.commons.cli.*;
 import java.io.FileInputStream;
